b0d02d1fa77bb555932afaaf9087ab5e2fe35106
kadyrkuloveA <emptyline03@gmail.com>
2020-01-30 17:13:49 +0600

    first

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+
+const app = express();
+const port = 8000;
+
+//task1
+app.get('/:string', (req, res) => {
+    res.send(req.params.string);
+});
+
+//task2
+const password = 'drip';
+
+app.get('/encode/:string', (req, res) => {
+    res.send(Vigenere.Cipher('password').crypt(req.params.string));
+});
+
+app.get('/decode/:string', (req, res) => {
+    res.send(Vigenere.Decipher('password').crypt(req.params.string));
+});
+
+
+app.listen(port, () => {
+    console.log('live on ' + port + ' port \n For Message http://localhost:' + port + '/something \n For encode http://localhost:' + port + '/something \n For decode http://localhost:' + port + '/something');
+});
